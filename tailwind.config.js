5b918218b2b3e8129da753bfb9220b81af3b33df
balodim29 <balooladimeji29@gmail.com>
2023-09-08 00:33:27 +0100

    gsgsg

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    screens:{
+    md:"768px",
+    lg:"1100px"
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
